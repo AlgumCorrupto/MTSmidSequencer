d2066f87f28e44b6c4964583e1c4edd7793f55f0
AlgumCorrupto <pauloarturvillaca@gmail.com>
2023-06-15 11:35:58 -0300

    Still on the works

--- MTSwebMIDIseq/src/app/keyboard/keyboard.component.ts
+++ MTSwebMIDIseq/src/app/keyboard/keyboard.component.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SynthClassService } from '../synth/synth-class.service';
+import { FreqbankInterface } from '../synth/FreqbankInterface';
 
 @Component({
   selector: 'app-keyboard',
   templateUrl: './keyboard.component.html',
   styleUrls: ['./keyboard.component.scss']
 })
-export class KeyboardComponent {
-  constructor(SynthService: SynthClassService){
+export class KeyboardComponent implements OnInit {
+  notes: FreqbankInterface[] = [];
+  currentNotes: FreqbankInterface[] = [];
+  currentOctave:number = 0;
+  nOffset: number = 0
+  constructor(private synthService: SynthClassService){ }
 
+  ngOnInit(): void {
+    this.currentNotes = this.setCurrentOctave(7);
   }
+
   getNotes(){
-    return
+    if(this.synthService.noteTable !== null)
+      this.notes = this.synthService.noteTable;
+  }
+
+  setCurrentOctave(octave:number): FreqbankInterface[] {
+    this.currentOctave = octave;
+
+    let offset: number | undefined;
+    let octaveBuffer: FreqbankInterface[] = []
+    for(let i = 0; i<this.notes.length; i++) {
+      if(this.notes[i].octave === octave) {
+        octaveBuffer.push(this.notes[i]);
+        if (offset === undefined)
+          offset = i;
+      }
+    }
+
+    if(offset !== undefined)
+      this.nOffset = offset;
+
+    return octaveBuffer;
   }
 }
--- MTSwebMIDIseq/src/app/synth/synth-class.service.ts
+++ MTSwebMIDIseq/src/app/synth/synth-class.service.ts
@@ -1,9 +1,54 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { FreqbankInterface } from './FreqbankInterface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SynthClassService {
+export class SynthClassService implements OnInit {
+  audioContext = new window.AudioContext()
+  oscList = [];
+  masterGainMode = null;
+  noteTable: FreqbankInterface[] = [];
+  inited: boolean = false;
+  init() {
+    if (!this.inited)
+      this.inited = true;
+  }
 
-  constructor() { }
+  generateEDOnoteTable(baseFreq:number, numOfIntervals:number){
+    let lowestOctave:number = baseFreq;
+    let highestOctave:number = baseFreq;
+    let threshold: number = 0;
+    let cont: number = 0;
+    let octaveBank: FreqbankInterface[] = [];
+    let noteBank: FreqbankInterface[] = [];
+    while(lowestOctave >= 20) {
+      lowestOctave /= 2;
+    }
+    threshold = lowestOctave;
+    while(highestOctave >= 22000) {
+      highestOctave *= 2;
+    }
+    while(threshold <= 22000) {
+      let valueBuffer: FreqbankInterface = {freq: threshold, octave: cont}
+      octaveBank.push(valueBuffer);
+      cont++;
+    }
+    for(let octave = 0; octave < octaveBank[cont-2].octave; octave++) {
+      for(let interval = 0; interval < numOfIntervals; interval++) {
+        let frequency = octaveBank[octave].freq * Math.pow(2, interval/numOfIntervals)
+        let valueBuffer: FreqbankInterface = {freq:frequency, octave: octave}
+        noteBank.push(valueBuffer);
+      }
+    }
+    return noteBank;
+  }
+
+  ngOnInit(){
+    this.noteTable = this.generateEDOnoteTable(440, 12);
+  }
+
+  constructor() {
+
+  }
 }
